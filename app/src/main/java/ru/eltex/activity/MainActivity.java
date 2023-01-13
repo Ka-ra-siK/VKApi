f2895f7b72ef1280487334168719975738830055
un1t0n <vadim.chernyavsky@yandex.ru>
2023-01-13 12:35:10 +0700

    add menu for fragments

--- app/src/main/java/ru/eltex/activity/MainActivity.java
+++ app/src/main/java/ru/eltex/activity/MainActivity.java
@@ -1,15 +1,24 @@
 package ru.eltex.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import ru.eltex.R;
 import ru.eltex.api_service_friends.WebClient;
+import ru.eltex.databinding.ActivityMainBinding;
+import ru.eltex.fragments.FriendsFragment;
+import ru.eltex.fragments.MessagesFragment;
+import ru.eltex.fragments.NewsFragment;
+import ru.eltex.fragments.UserFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     //ID of the VK application
     private static String APP_ID = "51509978";
@@ -17,13 +26,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main);
         WebView webView = findViewById(R.id.web_link);
-
         //Authorization request
         webView.loadUrl("https://oauth.vk.com/authorize?client_id=" + APP_ID + "&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,wall&response_type=token&v=5.74&display=mobile");
         webView.setWebViewClient(new WebClient(this));
+        replaceFragment(new UserFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.person_menu:
+                    replaceFragment(new UserFragment());
+                    break;
+                case R.id.news_menu:
+                    replaceFragment(new NewsFragment());
+                    break;
+                case R.id.message_menu:
+                    replaceFragment(new MessagesFragment());
+                    break;
+                case R.id.friends_menu:
+                    replaceFragment(new FriendsFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.web_link, fragment); // TODO not web_link
+        fragmentTransaction.commit();
     }
 
 }
